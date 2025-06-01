e30e20a8b7bb574d8c6e312590597ac9dd221054
Sean Han <seanmacbook@SeanMacbook.local>
2025-06-01 15:08:51 -0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+<<<<<<< Updated upstream
+=======
+import { fileURLToPath } from "url";
+import { dirname, resolve } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+>>>>>>> Stashed changes
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: resolve(__dirname, "client"),
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
